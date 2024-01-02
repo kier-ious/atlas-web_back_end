2cc7625c7a2f858ca6739ebe1e3aac555f2c4d04
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-02 12:17:00 -0600

    Task 0, instantiate vars using const and let

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,18 @@
+// Task 0
+
+
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
