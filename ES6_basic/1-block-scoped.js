40706eb283807b1a999e1b0ebd8e50dbcafe102b
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-02 16:43:11 -0600

    Included a IIFE function to create a new block-scoped context

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,5 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  if (trueOrFalse) {
+    (function() {
+      let task = true;
+      let task2 = false;
+    })();
+  }
+
   return [task, task2];
 }
