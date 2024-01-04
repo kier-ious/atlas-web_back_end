8f427667e20616682c2292256e5a9b5bdf4d2d62
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-04 12:55:15 -0600

    corrected some linting issues.

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,4 +1,5 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
   const sizeOfRooms = [19, 20, 34];
   const classrooms = sizeOfRooms.map(size => new ClassRoom(size));
