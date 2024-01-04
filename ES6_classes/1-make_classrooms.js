885408257a397de0a6174fd5e706460f7e89ed77
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-04 12:41:04 -0600

    task1, importing previous class, initializing function name and returning ordered array

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import ClassRoom from "./0-classroom";
+export default function initializeRooms() {
+  const sizeOfRooms = [19, 20, 34];
+  const classrooms = sizeOfRooms.map(size => new ClassRoom(size));
+  return classrooms;
+}
