45bdbe419828cd5bdbeb50d975881d8b1ea128f9
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-04 13:04:01 -0600

    corrected arrow parameter by adding () around size function

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,6 +2,6 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const sizeOfRooms = [19, 20, 34];
-  const classrooms = sizeOfRooms.map(size => new ClassRoom(size));
+  const classrooms = sizeOfRooms.map((size) => new ClassRoom(size));
   return classrooms;
 }
