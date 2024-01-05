452eeabaac8b7e6041b882a2085575b7e423c0b2
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-05 13:31:38 -0600

    Added TypeErros to the constructors but was able to remove thr initial constructors I had in place because the new if statements in the constructor handle initialization and the TypeErrors

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,22 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a valid number');
+    }
+
+    if (typeof students === 'object') {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 
   // Getter and Setter for NAME
@@ -37,7 +51,7 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    if (Array.isArray(newStudents) && newStudents.every((item) => typeof item === 'students')) {
+    if (typeof newStudents === 'object') {
       this._students = newStudents;
     } else {
       throw new TypeError('Students must be an array of strings');
