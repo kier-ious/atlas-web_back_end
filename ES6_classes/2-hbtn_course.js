fe9b4a3c1cbba6bd73447186d0d77539cbd25a59
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-05 13:09:11 -0600

    added TypeErrors to all setters and corrected my typo in one of them.

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -5,34 +5,42 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  // Getter and Setter for name
+  // Getter and Setter for NAME
   get name() {
     return this._name;
   }
 
   set name(newName) {
-    if (typeof newName === 'string');
+    if (typeof newName === 'string') {
       this._name = newName;
+    } else {
+      throw new TypeError('Name must be a string');
     }
+  }
 
-  // Getter and Setter for length
+  // Getter and Setter for LENGTH
   get length() {
     return this._length;
   }
 
   set length(newLength) {
-    if (typeof newLength === 'length');
+    if (typeof newLength === 'number') {
       this._length = newLength;
+    } else {
+      throw new TypeError('Length must be a valid number');
     }
+  }
 
-  // Getter and Setter for students
+  // Getter and Setter for STUDENTS
   get students() {
     return this._students;
   }
 
   set students(newStudents) {
-    if (typeof newStudents === 'students');
+    if (typeof newStudents === 'students') {
       this._students = newStudents;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
-
+  }
 }
