a75b1f06805d06f218256a8f2fa54d5094057f59
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-05 17:43:55 -0600

    task8, had already done task8 because I mixed up 7 and 8

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,26 +1,26 @@
-export default class Airport {
-  constructor(name, code) {
-    this._name = name;
-    this._code = code;
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
   }
 
   // Getter for NAME
-  get name() {
-    return this._name;
+  get size() {
+    return this._size;
   }
 
   // Getter for CODE
-  get code() {
-    return this._code;
+  get location() {
+    return this._location;
   }
 
   // Cast NUMBER
   valueOf() {
-    return this._name;
+    return this._size;
   }
 
   // Cast to STRING
   toString() {
-    return this._code;
+    return this._location;
   }
 }
