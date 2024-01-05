977fc3f56073b7238fc86c6c5decc89ced0d70e3
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-05 17:18:29 -0600

    task7, definition of how an object should repr when converted to an integer

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // Getter for SIZE
+  get size() {
+    return this._size;
+  }
+
+  // Getter for LOCATION
+  get location() {
+    return this._location;
+  }
+
+  // Cast NUMBER
+  valueOf() {
+    return this._size;
+  }
+
+  // Cast to STRING
+  toString() {
+    return this._location;
+  }
+}
