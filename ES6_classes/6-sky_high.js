6cf5ee5df2f235c269d251bf2b16a85e6a4835cc
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-05 16:44:40 -0600

    task6, importing Building from 5 to make SkyHigBuilding a child of the Building. Also having anoverwritten message

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building{
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Getter for FLOORS
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacute slowly the ${this._floors} floors.`;
+  }
+}
