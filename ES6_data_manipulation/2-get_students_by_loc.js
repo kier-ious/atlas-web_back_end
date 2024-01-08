46b7d4513b8a12c958f4394696083ae4ab61b9d8
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-08 14:54:51 -0600

    task2, using filter to create a function to return an array of objects who's students are located in a specific city.

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+function getStudentsByLocation(students, city) {
+  const studentsInCity = students.filter(student => student.location === city);
+  return studentsInCity;
+}
+
+const city = 'San Francisco';
+const studentsInCity = getStudentsByLocation(student, city);
