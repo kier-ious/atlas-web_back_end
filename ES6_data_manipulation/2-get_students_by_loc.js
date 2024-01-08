f06275975fdda3640e73847aab4f3ddd0a94bdbe
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-08 15:03:28 -0600

    Declared vars correctly.

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,10 @@
-function getStudentsByLocation(students, city) {
+import getListStudents from './1-get_list_student_ids';
+
+export default function getStudentsByLocation(students, city) {
   const studentsInCity = students.filter((student) => student.location === city);
   return studentsInCity;
 }
 
 const city = 'San Francisco';
-const studentsInCity = getStudentsByLocation(student, city);
+const students = getListStudents();
+const studentsInCity = getStudentsByLocation(students, city);
