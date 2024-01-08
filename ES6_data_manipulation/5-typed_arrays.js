55e033d8340a998e1e312dd2b76fa5c6522e0d94
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-08 16:53:46 -0600

    forgot to capitalizze Int8

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,8 +4,8 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
-  int8[position] = value;
+  const Int8 = new Int8Array(buffer);
+  Int8[position] = value;
 
   return buffer;
 }
