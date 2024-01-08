5429d026f9cada40eb014da5d35fd1801ad60d82
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-08 14:10:37 -0600

    task1, using mapping to returns an array of ids from a list of object

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentsIds() {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const studentsIds = students.map(student => student.id);
+  return studentsIds;
+}
