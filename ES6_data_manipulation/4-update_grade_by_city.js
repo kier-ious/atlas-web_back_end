7fd9b1cc9479ee5cb5d82c4a6425a39a2655615e
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-08 16:26:15 -0600

    task4, using filter and mao combined return array of students per city and new grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+import getListStudents from './0-get_list_students'
+
+export default function updateStudentGradeByCity(students) {
+  const filterStudents = students.filter((student) => student.location === city);
+  const updatedStudents = filterStudents.map((student) => {
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+    const grade = gradeObj ? gradeObj.grade : 'N/A';
+    return { ...student, grade };
+  });
+  return updatedStudents;
+}
