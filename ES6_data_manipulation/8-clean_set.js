8ad3c94d05ca3b2ba13396edb5b47617b8bd89a6
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-09 12:21:58 -0600

    The linter didn't like the backticks. I replaced them with single quotes

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
   }
   const filteredValues = Array.from(set)
     .filter((value) => value.startsWith(startString))
-    .map((value) => value.replace(startString, ``));
+    .map((value) => value.replace(startString, ''));
 
   const updatedString = filteredValues.join('-');
   return updatedString;
