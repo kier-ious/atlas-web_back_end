1ecafbb49ba43ce4708c630cb7dba4e3e6f12da1
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-08 18:22:15 -0600

    task9, returns a map of a grocery list with name of item and quantity

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const arrayFromSet = Array.from(set);
-  const filterSet = arrayFromSet.filter((value) => value.startsWith(startString));
-  const updatedString = filterSet.join('-');
+  const filteredValues = arrayFromSet.filter((value) => value.startsWith(startString));
+  const updatedString = filteredValues.join('-');
   return updatedString;
 }
