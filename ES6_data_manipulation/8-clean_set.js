f0507374d1c4c7b84fdd62061f815bd3966a4121
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-09 11:03:23 -0600

    Added if statement to check if string is empty or a string at all.

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,10 @@
 export default function cleanSet(set, startString) {
-  const arrayFromSet = Array.from(set);
-  const filteredValues = arrayFromSet.filter((value) => value.startsWith(startString));
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+  const arrayFromSet = Array.from(set)
+    .filter((value) => value.startsWith(startString));
+
   const updatedString = filteredValues.join('-');
   return updatedString;
 }
