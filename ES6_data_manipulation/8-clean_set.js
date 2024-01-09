2ff5213b74a6c5b6a263b5427a72caf232f00ddc
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-08 18:08:59 -0600

    converted set to an array and changed append to join on line 4

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(set, startString) {
-  const filterSet = Array.from(set).filter((value) => value.startsWith(startString));
-  const updatedString = filterSet.append('-');
+  const arrayFromSet = Array.from(set);
+  const filterSet = arrayFromSet.filter((value) => value.startsWith(startString));
+  const updatedString = filterSet.join('-');
   return updatedString;
 }
