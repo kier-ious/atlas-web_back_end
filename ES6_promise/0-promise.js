22b39ce2e5b2a20c34a35ae0165a3fe5b0153c29
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 12:41:39 -0600

    Corrected linting issues

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     fakeTimer(() => {
-      let success = true;
+      const success = true;
       if (success) {
-        resolve( 'API Respose Data');
+        resolve('API Respose Data');
       } else {
         reject(new TypeError('Failed to fetch data'));
       }
