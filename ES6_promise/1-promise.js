0354ca78131b7403f2ceee9cbead86770628682d
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 13:35:05 -0600

    removed the const success

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-      const success = true;
-      if (success) {
-        resolve({ success: 200, body: 'Success' });
-      } else {
-        reject(new TypeError('The fake API is not working currently'));
-      }
+    if (success) {
+      resolve({ success: 200, body: 'Success' });
+    } else {
+      reject(new TypeError('The fake API is not working currently'));
+    }
   });
 }
