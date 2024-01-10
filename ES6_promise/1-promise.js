93d6d5d5fda91f399336399e9ab09703aac092f0
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 14:17:48 -0600

    Made a const called myPromise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
+  const myPromise = new Promise((resolve, reject) => {
     if (success) {
       resolve({ success: 200, body: 'Success' });
     } else {
       reject(new TypeError('The fake API is not working currently'));
     }
   });
+  return myPromise;
 }
