ca5eb33b9aa2f3ddca991b23fed939a16bafaad0
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 18:56:43 -0600

    Added a wrap to call in the other promise and resolve right away

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then((results) =>
-      results.map((result) => ({
-        status: result.value,
-        value: result.value === 'fulfilled' ? result.value : result.reason,
-      }))
-    );
+  return new Promise((resolve) => {
+    Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ])
+      .then((results) =>
+        resolve(
+          results.map((result) => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : result.reason,
+          }))
+        )
+      );
+  });
 }
