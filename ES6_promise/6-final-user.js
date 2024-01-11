6f641e255c0dd490d5f4f3016b97007e9e5a324c
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 18:59:47 -0600

    Trimmed a lot of fat

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,18 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return new Promise((resolve) => {
-    Promise.allSettled([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ])
-      .then((results) =>
-        resolve(
-          results.map((result) => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason,
-          }))
-        )
-      );
-  });
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ])
 }
