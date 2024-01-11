ab41a5a6e751c5c193389524fcee65f1930a682d
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 18:38:53 -0600

    rewrote function, got the correct output in terminal

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const photoUploadPromise = uploadPhoto(fileName);
-
-  const results = await Promise.allSettled([signUpPromise, photoUploadPromise]);
-
-  return results.map((result) => ({
-    status: result.status,
-    value: result.value || result.reason,
-  }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+  .then((results) =>
+  results.map((results) => ({
+    status: results.value,
+    value: results.value || results.reason,
+  }))
+  );
 }
