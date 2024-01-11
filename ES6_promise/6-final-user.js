0d613faf7de6f5054be8cd845b7df9c949b25481
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 19:07:51 -0600

    corrected linting issues

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,12 +4,12 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
-  const photoUploadPromise = uploadPhoto(filename);
+  const photoUploadPromise = uploadPhoto(fileName);
 
   const results = await Promise.allSettled([signUpPromise, photoUploadPromise]);
 
   return results.map((results) => ({
     status: results.status,
-    value: results.value || results.reason
+    value: results.value || results.reason,
   }));
 }
