e9c135f4aebae9d4a8737ac818e5638db89e16c6
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 19:01:42 -0600

    Brought back the fat

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ])
+    .then((results) =>
+      results.map((result) => ({
+        status: result.value,
+        value: result.value === 'fulfilled' ? result.value : result.reason,
+      }))
+    );
 }
