a06ed6de623b4575c767b90656aebcbcac934f76
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 18:27:03 -0600

    task6, importing 2 previous files to return an array with provided function

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const photoUploadPromise = uploadPhoto(fileName);
+
+  const results = await Promise.allSettled([signUpPromise, photoUploadPromise]);
+
+  return results.map((results) => ({
+    status: result.status,
+    value: results.value || result.reason
+  }));
+}
