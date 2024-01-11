15c522d0300df67173c3c58e32af203328b6af79
Kier McAlister <126838794+kier-ious@users.noreply.github.com>
2024-01-10 19:06:53 -0600

    reverted back to an old commit

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
 import signUpUser from './4-user-promise';
+
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then((results) =>
-    results.map((result) => ({
-      status: result.value,
-      value: result.value || result.reason,
-  }))
-  );
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const photoUploadPromise = uploadPhoto(filename);
+
+  const results = await Promise.allSettled([signUpPromise, photoUploadPromise]);
+
+  return results.map((results) => ({
+    status: results.status,
+    value: results.value || results.reason
+  }));
 }
