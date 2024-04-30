7066175e27a5dffabe168e6c0dabb2afb5d2d88d
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-04-30 14:39:35 -0500

    task2, using import math so find the floor of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Takes a float n as argument and returns the floor of the float"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns floor of float"""
+    return math.floor(n)
