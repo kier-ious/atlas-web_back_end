adc0d90447845cf0d9ef23cf7ac60dbf8281f7fa
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-04-30 16:23:07 -0500

    added another docstring above return

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
