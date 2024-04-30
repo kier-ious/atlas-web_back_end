fbfe80019b9c4302f9cd3babea9a99d807164b2c
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-04-30 13:50:19 -0500

    added another docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Adding two floats to get the sum of the floats, will return float"""
+
+
 def add(a: float, b: float) -> float:
-    '''Adding two floats to get the sum of the floats, will return float'''
+    """return float"""
     return a + b
