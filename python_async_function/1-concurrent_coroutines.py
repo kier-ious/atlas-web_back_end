666d79f43cd5356c0f6c315fe2eb3edd2622fd21
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-04-30 17:38:09 -0500

    Added from typing import List and changed list to List in func

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Execute multiple coroutines at the same time with async"""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times with the specified max_delay
 
     Args:
