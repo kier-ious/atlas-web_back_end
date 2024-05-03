883581846ef8fe9e6681aa9136dcdd8475b5a799
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-03 16:40:01 +0000

    Added type annotations and return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,8 +2,15 @@
 """The comprehension of async"""
 import asyncio
 import random
+from typing import Generator
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
+    """Async generator that yields 10 random #'s between 0-10
+
+    Yields:
+        float: Random floating number between 0-10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
