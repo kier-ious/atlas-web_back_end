4e43ed8c02995a20da1095089049d01ecbe096ab
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-03 18:08:05 +0000

    added comment about the usage of asyncio.gather()

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -15,6 +15,7 @@ async def measure_runtime() -> float:
     """
     start_time = asyncio.get_event_loop().time()
 
+    # Execute prev mod 4 times in parallel using asyncio.gather
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
