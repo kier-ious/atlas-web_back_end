1cf97868a8d28e19be5d6535637aa85ff7d6c914
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-03 18:15:10 +0000

    added a generator expression within asyncio.gather for more concise listing

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,6 @@
 """The comprehension of async"""
 import asyncio
 import time
-
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
@@ -16,12 +15,7 @@ async def measure_runtime() -> float:
     start_time = asyncio.get_event_loop().time()
 
     # Execute prev mod 4 times in parallel using asyncio.gather
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
 
     end_time = asyncio.get_event_loop().time()
     return end_time - start_time
