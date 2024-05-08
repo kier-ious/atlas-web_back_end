ae31a5e1abe3e7e6bdc6172f4b91ac9319370330
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-08 00:37:26 +0000

    working on task3, got a similar output for LRU. pushing to see if it actually works

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""(LIFO Caching)Create a class LIFOCache that inherits from BaseCaching
-and is a caching system"""
+"""(LRU Caching)Create a class LRUCache that inherits from BaseCaching and is a caching system"""
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """Defines caching system with LIFO"""
+class LRUCache(BaseCaching):
+    """Defines caching system with LRU"""
     def __init__(self):
         super().__init__()
-        self.stack = []
+        self.usage_history = []
         self.size = 0
 
     def put(self, key, item):
@@ -23,18 +22,23 @@ class LIFOCache(BaseCaching):
             return
 
         if self.size >= self.MAX_ITEMS:
-            """If cache full kick first item out"""
-            old_key = self.stack.pop()
+            """If the cache is full kick out least used item"""
+            old_key = self.usage_history.pop(0)
             del self.cache_data[old_key]
             print("DISCARD:", old_key)
             self.size -= 1
 
         self.cache_data[key] = item
-        self.stack.append(key)
+
+        if key in self.usage_history:
+            self.usage_history.remove(key)
+        self.usage_history.append(key)
         self.size += 1
 
     def get(self, key):
         """Retrieves item from cache"""
         if key in self.cache_data:
+            self.usage_history.remove(key)
+            self.usage_history.append(key)
             return self.cache_data[key]
         return None
