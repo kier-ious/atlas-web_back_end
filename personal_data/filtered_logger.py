55f425338817ed9f5cd23ec6d86efd9d69b1ade1
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-14 19:33:45 +0000

    fix pep8 and added docstringzzz

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,19 @@ that returns the log message obfuscated"""
 import re
 
 
-def filter_datum(fields, redaction, message,separator):
+def filter_datum(fields, redaction, message, separator):
+    """Obfuscates specified fields in a log msg
+
+    Args:
+        fields (list): a list of strings representing all fields to obfuscate
+        redaction (str): a string representing by what the field will be
+            obfuscated
+        message (str): a string representing the log line
+        separator (str): a string representing by which character is separating
+            all fields in the log line (message)
+
+    Returns:
+        str: The log message w/ specified fields obfuscated
+    """
     pattern = '|'.join(fields)
     return re.sub(f'({pattern})=[^{separator}]*', f'\\1={redaction}', message)
