26104b7c5908a9d8e73a6e148189c545e8e52493
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-20 20:43:57 +0000

    task1, added error handler in api/v1/views

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'])
+def unauthorized_endpoint():
+    """New endpoint to raise unauthorized error"""
+    abort(401)
