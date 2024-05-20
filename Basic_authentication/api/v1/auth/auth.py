d47f528925ab05f5b152b5beabffcc93b4b56985
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-20 21:53:04 +0000

    updating reuore_auth method that returns true if path is not in list of strings

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,10 +8,21 @@ from typing import List, TypeVar
 class Auth():
     """This class is the template for
     all authentication system you will implement."""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """eturns False - path and excluded_paths will be used
+        """Returns False - path and excluded_paths will be used
         later, now, you don’t need to take care of them"""
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return None
+        normalized_path = path if path.endswith('/') else path + '/'
+        for excl_path in excluded_paths:
+            normalized_excl_path = excl_path \
+                if excl_path.endswith('/') else excl_path + '/'
+            if normalized_path == normalized_excl_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns none-request will be flask request object"""
