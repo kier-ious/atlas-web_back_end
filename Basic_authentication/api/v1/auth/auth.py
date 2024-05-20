94ca0cd14083ed32670712257316fa3d4671f36c
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-20 21:59:43 +0000

    Adjusted excluded_paths is none and had it return true

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -14,8 +14,8 @@ class Auth():
         later, now, you don’t need to take care of them"""
         if path is None:
             return True
-        if not excluded_paths:
-            return False
+        if not excluded_paths is None:
+            return True
         normalized_path = path if path.endswith('/') else path + '/'
         for excl_path in excluded_paths:
             normalized_excl_path = excl_path \
