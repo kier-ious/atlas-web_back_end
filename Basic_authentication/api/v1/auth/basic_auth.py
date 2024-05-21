67b07d8f99f577263d8b4e6ce53a4b670d5c006c
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-21 03:31:23 +0000

    added extract_base64_authorization_header to basicauth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Child class inheriting from parent, currently empty"""
     pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Returns the Base64 part of Auth header for BasicAuth"""
+        if authorization_header is None or \
+            not isinstance(authorization_header, str) or \
+            not authorization_header.startswith('Basic '):
+                return None
+
+        return authorization_header.split(' ')[1]
