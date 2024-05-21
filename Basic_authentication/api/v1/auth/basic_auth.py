efdf878a3cb8e7ed567b7c75b244fa7012703e52
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-21 03:18:45 +0000

    ok, figured out file stack lol

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Child class of Auth"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Child class inheriting from parent, currently empty"""
+    pass
