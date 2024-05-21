e61e85460ca6c5cfb19ad24019cc0748bcd9985b
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-21 16:27:37 +0000

    task10, returns the User instance based on his email and password.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Child class of Auth"""
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -46,3 +47,24 @@ class BasicAuth(Auth):
 
         user_credintials = decoded_base64_authorization_header.split(':', 1)
         return user_credintials[0], user_credintials[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Returns the User instance based on his email and password."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        """Begin user search in DB"""
+        user = User.search({'email': user_email})
+        if not user:
+            return None
+
+        user = user[0]
+
+        """Checking PW is valid"""
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
