32d4640040eb2a39a7c558f6d6d91a083c937474
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-23 16:00:57 +0000

    created sessions_auth.py (child class of auth = pass) and adding logic

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -18,6 +19,8 @@ auth_type = getenv('AUTH_TYPE')
 
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
 else:
     auth = Auth()
 
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""DOCDOCDOCDOC"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """1st step to create authorization mechanism"""
+    pass
