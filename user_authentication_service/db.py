0de8478eab312c5442fd9c0b33773d4c7737830c
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-25 23:31:20 +0000

    task2, adding user to db

---
+++ user_authentication_service/db.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""DataBase"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds user to the DB
+
+        Args:
+            email (str): the users email
+            hashed_password (str): the users PW
+
+        Returns:
+            User: info/object
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        """Add user to session"""
+        self._session.add(new_user)
+
+        self._session.commit()
+
+        return new_user
