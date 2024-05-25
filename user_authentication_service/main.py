437d8f502d8196d307d27862e2480c380f025504
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-25 23:37:09 +0000

    made changed to main.py in the print statements

--- user_authentication_service/main.py
+++ user_authentication_service/main.py
@@ -9,7 +9,7 @@ from user import User
 my_db = DB()
 
 user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
+print("DB.add_user returns a user object:", isinstance(user_1, User))
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+print("DB.add_user returns a user object:", isinstance(user_2, User))
