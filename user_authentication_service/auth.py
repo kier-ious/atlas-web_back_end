9e12662d15a02849731e86d7b25bbb7ce1a8b107
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 02:45:36 +0000

    removed redundant imports

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 """Auth"""
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from user import Base, User
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 import bcrypt
 from db import DB
 
