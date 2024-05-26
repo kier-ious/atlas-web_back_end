7840321f71812963e313a4f35bcf2d741cc1b42a
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 03:02:36 +0000

    fixing importzz

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.orm.exc import InvalidRequestError, NoResultFound
 
 
 class DB:
