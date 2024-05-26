d99d03919f902b2e2e3efb38d2450dea41d0d086
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 19:43:50 +0000

    Corrected the raise error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -102,7 +102,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            raise ValueError(f"User with email {email} does not exist")
+            raise ValueError("User DNE")
 
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
