1a53ddb5f1b7721a3cfec141684698a5b5361a3f
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 04:45:45 +0000

    added route to app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """the APP"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 
@@ -24,6 +24,23 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(400, 'Email and password are required')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({'email': email, 'message': 'logged in'})
+        response.set_cookie('session_id', session_id)
+        return response, 200
+    else:
+        abort(401, 'Unauthorized')
 
 
 if __name__ == "__main__":
