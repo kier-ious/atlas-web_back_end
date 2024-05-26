7c7aea1e13b677fc863770ea8df6a332d341b472
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 19:47:14 +0000

    took out print statment

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -103,7 +103,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             raise ValueError("User DNE")
-        print("User DNE")
 
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
