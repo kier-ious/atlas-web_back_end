a0e9e742a278499c87764f8a00f754c109960317
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 01:24:53 +0000

    corrected var names and typos

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -19,9 +19,9 @@ class Auth:
     @staticmethod
     def _hash_password(password: str) -> bytes:
         """Returns bytes in a salty hash of input PW"""
-        _hash_password = bcrypt.hashpw(password.encode(
+        hashed_password = bcrypt.hashpw(password.encode(
             'utf-8'), bcrypt.gensalt())
-        return _hash_password
+        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """Register a new user"""
@@ -31,8 +31,6 @@ class Auth:
 
         """If new, hash dat PW!"""
         hashed_password = self._hash_password(password)
-
         """Save into DB"""
         user = self._db.add_user(email, hashed_password)
-
         return user
