1c05945792ce9e3ffc7051428bba91fcd7cb4b03
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 01:47:24 +0000

    tsk7, added new route for register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """the APP"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -8,6 +12,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
