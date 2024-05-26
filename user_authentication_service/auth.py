a3e2fd07f08ffa5ede59b57720fbe3099aa576aa
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 18:58:23 +0000

    task13, destroys user session by setting to none

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -89,3 +89,10 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys user session by setting session ID to None"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            pass
