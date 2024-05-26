6d41e7e6ba7b7e5ce5fa9bfd4a76990303ff334e
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-26 20:08:13 +0000

    task18, update PW

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -107,3 +107,14 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update users PW w/ reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError("invalid reset token")
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(
+            user.id, hashed_password=hashed_password, rest_token=None)
