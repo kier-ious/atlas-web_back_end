e2fd009f08c4b36bad0a1b05beca9c01f5eb56df
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-28 20:34:12 +0000

    ksjdnvklsjd

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -85,7 +85,7 @@ class TestGithubOrgClient(unittest.TestCase):
         # Test case 1, repo w/ expected license key
         ({'license': {'key': 'other_license'}}, 'my license', True),
         # Test case 2, repo w/ different license key
-        ({'license': {'key': 'other_license'}}, 'my license', False),
+        ({'license': {'key': 'other_license'}}, 'my license', False)
     ])
     def test_has_license(self, repo, license_key, expected_result):
         """Test that has_license method of the GithubOrgClient class"""
