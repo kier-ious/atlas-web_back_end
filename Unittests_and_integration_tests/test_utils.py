35d426974d938743937706752ad7dea0f6dc870e
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-28 18:19:19 +0000

    task1,  implement keyerror exception

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -13,9 +13,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(
-        self, nested_map: Mapping, path: Sequence, expected: Any):
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand({
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    })
+    def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence):
+        with self.assertRaises(KeyError) as ke:
+            access_nested_map(nested_map, path)
+            """Verify the exception msg"""
+            self.assertEqual(str(ke.exception), str(path[-1]))
 
 
 if __name__ == '__main__':
