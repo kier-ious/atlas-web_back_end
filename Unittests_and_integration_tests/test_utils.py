39f958a67692cd4d14969a2aa9378e2e0aa055bf
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-05-28 19:07:51 +0000

    task3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,8 @@
 import unittest
 from parameterized import parameterized
 from typing import Any, Mapping, Sequence
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch, Mock
-from utils import get_json
 from typing import Dict
 
 
@@ -68,6 +67,34 @@ class TestGetJson(unittest.TestCase):
             """Assert that the GET method was called exactly once
             with the test_url"""
             mock_get.assert_called_once_with(test_url)
+
+class TestClass:
+
+    def a_method(self):
+        return 42
+
+    @memoize
+    def a_property(self):
+        return self.a_method()
+
+class TestMemorize(unittest.TestCase):
+    def test_memoize(self):
+        """Test that memoizes decorator caches the result
+        of a_method
+
+        Mocks:
+        TestClass.a_method to vrify its called only oonce.
+        """
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test_instance = TestClass()
+            """Acces a_property twice"""
+            first_call = test_instance.a_property
+            second_call = test_instance.a_property
+            """Assert that the property returns the correct value"""
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            """Assert that a_method is called only once"""
+            mock_method.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
