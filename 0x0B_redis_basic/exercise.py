1dee55c8e49e370d68c01886ce33f8d91202f32f
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-04 23:31:09 +0000

    corrected another typo

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -8,17 +8,17 @@ class Cache:
     """Simple cache class using Redis for storing and
     retrieving data"""
 
-    def __init__(self, host='locaalhost', port=6379, db=0):
+    def __init__(self, host='localhost', port=6379, db=0):
         """Initializes the Cache class
 
-        Args:
+        Args
             host (str, optional): Hostname or IP of the redis server
             port (int, optional): The port that Redis server is listening.
             Defaults to 6379.
             db (int, optional): Redis DB to use Defaults to 0.
         """
         self._redis = redis.Redis(host=host, port=port, db=db)
-        # self.flushdb()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Stores data in the cache and returns generated key
