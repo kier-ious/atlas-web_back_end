0b2a936c118d1cf9838a70053dd04c37db1b40c2
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-06 15:49:29 +0000

    added new docstring to the for loop in replay

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -47,6 +47,7 @@ def replay(method: Callable):
     """Print the call history"""
     print(f"{method.__qualname__} was called{len(inputs)} times:")
     for input_, output in zip(inputs, outputs):
+        """Decode input/output from bytes to str and print the details"""
         print(f"{method.__qualname__}(*{input_.decode(
             'utf-8')}) -> {output.decode('utf-8')}")
 
