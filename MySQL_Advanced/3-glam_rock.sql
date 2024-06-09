62d6dc0506f9919f784964a88a91c8db0227354b
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-09 02:15:28 +0000

    reverting back to my old code seeing if it helps

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,12 @@
 -- Task3, Glam rock
 /* Write a SQL script that lists all bands with Glam
 rock as their main style, ranked by their longevity */
-
-SELECT band_name,formed, split FROM metal_bands WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT
+    band_name,
+    IFNULL(YEAR(split) - YEAR(formed), 0) AS lifespan
+FROM
+    metal_bands
+WHERE
+    style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
