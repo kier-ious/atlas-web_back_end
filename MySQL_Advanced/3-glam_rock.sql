d4b0f018bdcab98f159b47461c87bc7a1e3cdbbc
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-09 02:10:46 +0000

    task3, glam rock ranked

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,13 @@
+-- Task3, Glam rock
+/* Write a SQL script that lists all bands with Glam
+rock as their main style, ranked by their longevity */
+
+SELECT
+  band_name,
+  IFNULL((YEAR(split) - YEAR(formed)), 0) AS lifespan
+FROM
+  metal_bands
+WHERE
+  style = 'Glam rock'
+ORDER BY
+  lifespan DESC;
