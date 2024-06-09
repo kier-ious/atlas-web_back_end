d77dd2b51659bab854f21485d4405564bd96fe59
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-09 00:02:35 +0000

    forgot the s on users

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- 0-uniq_users.sql
 -- SQL script that creates a table users if it doesn't exist
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL,
   name VARCHAR(255),
