103a67111d25d1cd7ce119101f1cc11565c5ecf5
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 16:29:56 -0500

    task5, email validation

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Task 5, Email validation
+-- SQL script to create trigger
+DELIMITER //
+CREATE TRIGGER reset_valid_email_on_change
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+      SET NEW.valid_email = 0;
+    END IF;
+END //
+
+DELIMITER;
