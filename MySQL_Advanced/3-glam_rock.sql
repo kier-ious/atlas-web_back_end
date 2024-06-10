033750ebec8ef59bebfea1ecde619a6d58dc3f28
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 20:29:47 +0000

    added 'name AS' before band_name

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 /* Write a SQL script that lists all bands with Glam
 rock as their main style, ranked by their longevity */
 SELECT
-    band_name,
+    name AS band_name,
     IFNULL(YEAR(COALESCE(split, CURDATE())) - YEAR(formed), 0) AS lifespan
 FROM
     metal_bands
