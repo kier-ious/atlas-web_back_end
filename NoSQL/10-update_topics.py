dead17c5b7df687956e8c46218fc324984e711c3
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-11 16:51:06 -0500

    task10, updating all docs that fit reqiuirement

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Update specific topic"""
+    query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+    mongo_collection.update_many(query, new_values)
