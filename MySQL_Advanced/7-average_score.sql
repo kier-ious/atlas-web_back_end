ba8190d72a87dfe28180792601275abdb5ca3ad5
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 19:27:24 -0500

    task7, script to store avg score for student

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Task 7, Average score
+-- Script that creates a stores scored average
+DELIMITER //
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+//
+CREATE PROCEDURE ComputeAverageScoreForUser(
+  IN user_id INT
+)
+BEGIN
+  DECLARE avg_score FLOAT;
+  -- Calculate the average score for the given user_id
+  SELECT AVG(score) INTO avg_score
+  FROM corrections
+  WHERE user_id = user_id;
+  -- Update the average_score in the users table
+  UPDATE users
+  SET average_score = avg_score
+  WHERE id = user_id;
+END //
+DELIMITER;
