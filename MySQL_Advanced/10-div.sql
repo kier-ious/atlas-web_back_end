1143ee56dba53e56bc14e92c9e3099fb8c36feed
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 21:09:54 -0500

    revering back to having it return a float

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -2,10 +2,8 @@
 -- Script that creates a function that divides and returns something
 DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 10)
-DETERMINISTIC
+RETURNS FLOAT
 BEGIN
-  DECLARE result DECIMAL(10, 10);
   IF b = 0 THEN
     SET result = 0;
   ELSE
