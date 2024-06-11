e11823a0ae1504d58314c3d1041ad4794a612541
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 21:25:38 -0500

    playing around with task 3 again, pushing to see if it worked

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -6,7 +6,5 @@ SELECT
     IFNULL(YEAR(COALESCE(split, CURDATE())) - YEAR(formed), 0) AS lifespan
 FROM
     metal_bands
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    lifespan DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
