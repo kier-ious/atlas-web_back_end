c00cd1e3e203f591e42ea0324a7fb2aad51408b1
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 20:06:47 -0500

    Had a typo in script, i had SELECT when it should have been CREATE

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 -- Task 8, Optimize simple search
 -- SCript that creates index in names table and 1st letter of name
-SELECT INDEX idx_name_first ON names(name(1));
+CREATE INDEX idx_name_first ON names(name(1));
