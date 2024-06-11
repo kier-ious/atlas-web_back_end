bcb5b89897f8c32ef45fce3fa2ae05a0e6cb3953
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 19:33:39 -0500

    removed the delimiter

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- Task 7, Average score
 -- Script that creates a stores scored average
-DELIMITER //
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(
   IN user_id INT
 )
@@ -10,10 +10,9 @@ BEGIN
   -- Calculate the average score for the given user_id
   SELECT AVG(score) INTO avg_score
   FROM corrections
-  WHERE user_id = user_id;
+  WHERE user_id = in_user_id;
   -- Update the average_score in the users table
   UPDATE users
   SET average_score = avg_score
-  WHERE id = user_id;
+  WHERE id = in_user_id;
 END //
-DELIMITER;
