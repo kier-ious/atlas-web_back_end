ea726571edd8a4604298521691717aeac89e2220
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-10 21:26:48 -0500

    sjkfngls

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
 -- Task3, Glam rock
 /* Write a SQL script that lists all bands with Glam
 rock as their main style, ranked by their longevity */
-SELECT
-    band_name,
-    IFNULL(YEAR(COALESCE(split, CURDATE())) - YEAR(formed), 0) AS lifespan
-FROM
-    metal_bands
+SELECT band_name, IFNULL(YEAR(COALESCE(split, CURDATE())) - YEAR(formed), 0) AS lifespan FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
