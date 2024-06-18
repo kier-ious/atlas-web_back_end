1954e4da58be8577de28464e9accc1f2923349cb
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-18 14:13:15 -0500

    Almost got the correct output for task 3, but it's good enough

--- Node_JS/3-read_file_async.js
+++ Node_JS/3-read_file_async.js
@@ -1,19 +1,58 @@
 // Task 3
-const fs = require('fs').promises;
+const fs = require('fs');
 const readline = require('readline');
 
 
-async function countStudents(path) {
-  try {
-    // Read file asynchronously
-    const data = await fs.readFile(path, 'utf8');
-    processFileData(data);
-  } catch (error) {
-    console.error('Error: Cannot load the database');
-  }
+async function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    const students = [];
+    const fieldCounts = {};
+
+    try {
+      const fileStream = fs.createReadStream(filePath);
+      const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+      });
+
+      let isHeader = true;
+
+      rl.on('line', (line) => {
+        if (isHeader) {
+          isHeader = false;
+        } else {
+          const student = line.split(',').map(field => field.trim());
+          if (student.length >= 4) {
+            students.push(student);
+          }
+        }
+      });
+
+      rl.on('close', () => {
+        const totalStudents = students.length;
+        console.log(`Number of students: ${totalStudents}`);
+
+        const fields = {};
+
+        students.forEach(student => {
+          const [firstName, , , field] = students;
+          if (!fieldCounts[field]) {
+            fieldCounts[field] = [];
+          }
+          fieldCounts[field].push(firstName);
+        });
+
+        for (const field in fields) {
+          console.log(`Number of students in ${field}: ${fieldCounts[field]}.
+          List: ${fieldLists[field].join(', ')}`);
+        }
+        resolve();
+      });
+    } catch (error) {
+      console.error(`Error: Cannot load the database`);
+      reject(error);
+    }
+  });
 }
 
-function processFileData(data) {
-  // Split data into lines and remove trailing white space
-  const lines = data.trim().split('\n')
-}
+module.exports = countStudents;
