98c6335114874c4e6b32456eef923fd7c7d0e72b
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-22 16:02:57 -0500

    added stub to task 3's file that are in 4 now

---
+++ unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+// Task 4, Stubs 🎟️
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+}
+
+module.exports = sendPaymentRequestToApi;
---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const expect = chai.expect;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment.js');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let calculateNumber;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.spy(Utils, 'calculateNumber').returnValues(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with SUM 100, 20',function () {
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    expect(calculateNumberStub.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+  });
+});
