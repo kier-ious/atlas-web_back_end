5d6b1b19f9c8d92f4e5dc48324bb118b34170001
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-22 23:13:40 -0500

    ...

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -7,12 +7,12 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
+app.get(/^\/cart\/(\d+)$/, (req, res) => {
   const id = req.params.id;
   res.send(`Payment methods for: ${id}`);
 });
 
-app.listen(7865, () => {
+app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
 
--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,25 +1,20 @@
-const request = require('request');
 const { expect } = require('chai');
-
-chai.use(chaiHttp);
+const chai = require('chai');
 
 describe('Cart page', function() {
   it('Correct status code when :id is a number?', function(done) {
-    import('chai-http').then(chaiHttp => {
-      const request = chaiHttp.default.request;
-      request('http://localhost:7865/cart/12', function (error, response) {
-        if (error) return done(error);
-        expect(response.statusCode).to.equal(200);
-        done();
-    });
-  })
-});
+      chai.request('http://localhost:7865/cart/12', function (error, response) {
+          if (error) return done(error);
+          expect(response.statusCode).to.equal(200);
+          done();
+      });
+  });
 
-it('Correct status code when :id is NOT a number (=> 404)?', function(done) {
-    request('http://localhost:7865/cart/hello', function (error, response) {
-        if (error) return done(error);
-        expect(response.statusCode).to.equal(404);
-        done();
-    });
+  it('Correct status code when :id is NOT a number (=> 404)?', function(done) {
+      chai.request('http://localhost:7865/cart/hello', function (error, response) {
+          if (error) return done(error);
+          expect(response.statusCode).to.equal(404);
+          done();
+      });
   });
 });
