f50d5c7a9ecca15ed30839572c903a1c0ee64a3d
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-23 17:05:43 -0500

    I have one test passing, getting correct id passed through

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,20 +1,23 @@
-const chai = require('chai');
-const { expect } = chai;
+const request = require('request');
+const { expect } = require('chai');
 
 describe('Cart page', function() {
-  it('Correct status code when :id is a number?', function(done) {
-      chai.request('http://localhost:7865/cart/12', function (error, response) {
-          if (error) return done(error);
-          expect(response.statusCode).to.equal(200);
-          done();
-      });
-  });
+    describe('GET /cart/:id', function() {
+        it('should return 200 when :id is a number', function(done) {
+            const id = 12;
+            request(`http://localhost:7865/cart/${id}`, function (error, response) {
+                if (error) return done(error);
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
 
-  it('Returns 404 when :id is NOT a number', function(done) {
-      chai.request('http://localhost:7865/cart/hello', function (error, response) {
-          if (error) return done(error);
-          expect(response.statusCode).to.equal(404);
-          done();
-      });
-  });
+    it('should return 404 when :id is NOT a number', function(done) {
+        chai.request('http://localhost:7865/cart/hello', function (error, response) {
+            if (error) return done(error);
+            expect(response.statusCode).to.have.status(404);
+            done();
+        });
+    });
 });
