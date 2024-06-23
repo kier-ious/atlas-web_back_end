80b8e9ff9faa8ea28d41f95410550791e2609cbc
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-22 22:58:56 -0500

    I've reverted my code several times and moved something around in the test file.

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,24 +1,25 @@
 const request = require('request');
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const expect = chai.expect;
+const { expect } = require('chai');
 
 chai.use(chaiHttp);
 
 describe('Cart page', function() {
   it('Correct status code when :id is a number?', function(done) {
+    import('chai-http').then(chaiHttp => {
+      const request = chaiHttp.default.request;
       request('http://localhost:7865/cart/12', function (error, response) {
-          if (error) return done(error);
-          expect(response.statusCode).to.equal(200);
-          done();
-      });
-  });
+        if (error) return done(error);
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+  })
+});
 
-  it('Correct status code when :id is NOT a number (=> 404)?', function(done) {
-      request('http://localhost:7865/cart/hello', function (error, response) {
-          if (error) return done(error);
-          expect(response.statusCode).to.equal(404);
-          done();
-      });
+it('Correct status code when :id is NOT a number (=> 404)?', function(done) {
+    request('http://localhost:7865/cart/hello', function (error, response) {
+        if (error) return done(error);
+        expect(response.statusCode).to.equal(404);
+        done();
+    });
   });
 });
