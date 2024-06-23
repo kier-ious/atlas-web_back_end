8e9bb7ec61bd596c23030729d6ce0836e9757359
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-22 23:29:06 -0500

    000

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,4 +1,5 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const { expect } = chai;
 
 describe('Cart page', function() {
   it('Correct status code when :id is a number?', function(done) {
@@ -9,7 +10,7 @@ describe('Cart page', function() {
       });
   });
 
-  it('Correct status code when :id is NOT a number (=> 404)?', function(done) {
+  it('Returns 404 when :id is NOT a number', function(done) {
       chai.request('http://localhost:7865/cart/hello', function (error, response) {
           if (error) return done(error);
           expect(response.statusCode).to.equal(404);
