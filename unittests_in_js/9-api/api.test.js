e8468e9610e86a1afb4abf5f1505e89fc14c6176
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-24 14:33:29 -0500

    added task 8's test

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -4,6 +4,23 @@ const app = require('./api.js');
 
 // chai.use(chaiHttp);
 // const { expect } = chai;
+describe('Index page', function() {
+    it('Correct status code?', function(done) {
+        request('http://localhost:7865/', function (error, response) {
+            if (error) return done(error);
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Correct result?', function(done) {
+        request('http://localhost:7865/', function (error, response, body) {
+            if (error) return done(error);
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+  });
 
 describe('Payment System', () => {
     describe('GET /', () => {
