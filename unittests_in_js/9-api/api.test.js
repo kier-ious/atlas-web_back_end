a5cd0bf49b4918d7739dfe34c4163b3c3d8331b8
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-24 13:07:59 -0500

    added arrow functions to test suite

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -5,10 +5,10 @@ const app = require('./api.js');
 // chai.use(chaiHttp);
 // const { expect } = chai;
 
-describe('Cart page', function() {
-    describe('GET /', function() {
-        it('should return 200 and welcome message', function(done) {
-            request('http://localhost:7865/', function (error, response, body) {
+describe('Payment System', () => {
+    describe('GET /', () => {
+        it('should return 200 and welcome message', (done) => {
+            request('http://localhost:7865/', (error, response, body) => {
                 if (error) return done(error);
                 expect(response.statusCode).to.equal(200);
                 expect(body).to.equal('Welcome to the payment system');
@@ -18,17 +18,18 @@ describe('Cart page', function() {
     });
 
     describe('GET /cart/:id', function() {
-        it('should return 200 when :id is a number', function(done) {
+        it('should return 200 when :id is a number', (done) => {
             const id = 12;
-            request(`http://localhost:7865/cart/${id}`, function (error, response) {
+            request(`http://localhost:7865/cart/${id}`, (error, response, body) => {
                 if (error) return done(error);
                 expect(response.statusCode).to.equal(200);
+                expect(body).to.equal(`Payment methods for cart ${id}`);
                 done();
             });
         });
 
-        it('should return 404 when :id is NOT a number', function(done) {
-            request('http://localhost:7865/cart/hello', function (error, response) {
+        it('should return 404 when :id is NOT a number', (done) => {
+            request('http://localhost:7865/cart/hello', (error, response, body) => {
                 if (error) return done(error);
                 expect(response.statusCode).to.equal(404);
                 done();
