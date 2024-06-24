6fd693ed12ce32bdd07550cff242ea82f008df13
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-24 12:50:51 -0500

    added 'GET' route in the test suite

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -6,6 +6,17 @@ const app = require('./api.js');
 // const { expect } = chai;
 
 describe('Cart page', function() {
+    describe('GET /', function() {
+        it('should return 200 and welcome message', function(done) {
+            request('http://localhost:7865/', function (error, response, body) {
+                if (error) return done(error);
+                expect(response.statusCode).to.equal(200);
+                expect(body).to.equal('Welcome to the payment system');
+                done();
+            });
+        });
+    });
+
     describe('GET /cart/:id', function() {
         it('should return 200 when :id is a number', function(done) {
             const id = 12;
