abd42be4cd43376c6226801f7721c4f5fa6d9039
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-25 11:48:35 -0500

    added to my 9-api imports

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -1,17 +1,23 @@
 // Task 9, Regex Integration testing
 const express = require('express');
 const app = express();
+const port = 7865;
+const host = 'localhost';
 
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id(\\d+)', (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id)) {
+    return res.status(404).send('ID must be a number');
+  }
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 
-app.listen(7865, () => {
-  console.log(`API available on localhost port 7865`);
+app.listen(port, () => {
+  console.log(`API available on ${host} port ${port}`);
 });
 
 module.exports = app;
