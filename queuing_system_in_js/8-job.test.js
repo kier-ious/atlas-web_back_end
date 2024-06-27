507b26c92db83eb898cefa667569eea5341a11e1
kier-ious <126838794+kier-ious@users.noreply.github.com>
2024-06-27 14:57:44 -0500

    finsihed. I'm so tried, I love it but I need to sleep

---
+++ queuing_system_in_js/8-job.test.js
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+import kue from 'kue';
+import createPushNotificationsJobs from '../8-job.js';
+
+const queue = kue.createQueue();
+
+describe('createPushNotificationsJobs', () => {
+  before(() => {
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+  });
+
+  after(() => {
+    queue.testMode.exit();
+  });
+
+  it('should create new jobs successfully', () => {
+    const jobs = [
+      { phoneNumber: '4153518780', message: 'This is the code 1234 to verify your account' },
+      { phoneNumber: '4153518781', message: 'This is the code 4562 to verify your account' }
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data).to.deep.equal(jobs[0]);
+  });
+
+  it('should throw an error if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs({}, queue)).to.throw('Jobs is not an array');
+  });
+});
